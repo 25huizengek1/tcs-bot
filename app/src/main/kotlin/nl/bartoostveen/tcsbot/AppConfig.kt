73cef09406f9e7b3ca7da01ebddc3afb194a29f1
Bart Oostveen <bart.oos.2006@gmail.com>
2025-11-09 03:00:42 +0100

    feat: add cache to http client

--- app/src/main/kotlin/nl/bartoostveen/tcsbot/AppConfig.kt
+++ app/src/main/kotlin/nl/bartoostveen/tcsbot/AppConfig.kt
@@ -5,6 +5,7 @@ import io.github.crackthecodeabhi.kreds.connection.newClient
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
@@ -147,6 +148,7 @@ object AppConfig {
           retryOnTimeout = true
         )
       }
+      install(HttpCache)
     }
   }
 
